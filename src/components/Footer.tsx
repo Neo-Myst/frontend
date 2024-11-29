eda6d47be3efe12fe9e8104315cd53e0cb61d224
Dhruv Vaghasiya <dhruvvaghasiya52@gmail.com>
2024-11-28 20:38:45 -0500

    fix: typing error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="text-center py-4 border-t border-gray-700 w-full relative bottom-0 top-10">
       <p className="text-gray-400 text-sm">
-        © 2024 ML Group 6. All rights reserved.s
+        © 2024 ML Group 6. All rights reserved.
       </p>
     </footer>
   );
