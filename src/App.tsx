d15fbe96c811c52872d448ac384d1fccb2c8187e
Dhruv Vaghasiya <dhruvvaghasiya52@gmail.com>
2024-12-14 20:21:24 -0500

    fix: Home Page Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import GoogleLogin from "./pages/GoogleLogin";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/google-login" element={<GoogleLogin />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/google-login" element={<GoogleLogin />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,31 @@
 import "../css/Nav.css";
+import { useNavigate } from "react-router-dom"; // Import useNavigate for navigation
 
 const Nav = () => {
+  const navigate = useNavigate();
   return (
     <nav className="fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4">
-      <div className="text-4xl font-bold text-white">
+      <div
+        className="text-4xl font-bold text-white"
+        onClick={() => navigate("/")}
+      >
         ML<span className="text-cyan-500">One</span>
       </div>
 
       <div className="flex items-center space-x-6">
         <a
+          onClick={() => navigate("/login")}
           href="#"
           className="text-white hover:text-cyan-500 transition duration-300"
         >
           Log In
         </a>
 
-        <button className="px-4 py-2 bg-cyan-600 text-white font-semibold rounded-md shadow-md hover:bg-cyan-700 transition duration-300">
-          Get Started
+        <button
+          className="px-4 py-2 bg-cyan-600 text-white font-semibold rounded-md shadow-md hover:bg-cyan-700 transition duration-300"
+          onClick={() => navigate("/signup")}
+        >
+          Sign up
         </button>
       </div>
     </nav>
