6ce0aed4ecfa08b284d411c2b3800cc671f6358e
Vedant-Mhatre <vedantmh@gmail.com>
2024-12-16 11:51:13 +0530

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import UserProvider from "./contexts/UserProvider.tsx";
+import React from "react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
