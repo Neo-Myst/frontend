a1d32899df9d2f8258ce53c643cf7573452ad49d
Vedant-Mhatre <vedantmh@gmail.com>
2025-03-24 16:53:32 -0400

    removed unnecessary file

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom", // Use JSDOM for React testing
-    setupFiles: "./setupTests.ts", // Load Jest DOM matchers before tests
-  },
-});
